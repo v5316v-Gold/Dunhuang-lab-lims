d4df7659eda8e164b146b5723c2f01b617e9f928
LIMS-Architect-01 <lims-architect@dunhuang.cn>
2026-08-15 19:08:17 +0800

    chore: 移除调试脚本(logtest.js / test-dpdf.js)

--- apps/backend/logtest.js
+++
@@ -1,4 +0,0 @@
-
-const { Logger } = require('@nestjs/common');
-const logger = new Logger('Test');
-try { logger.error('msg', undefined); console.log('ok'); } catch (e) { console.log('FAIL:', e.message); }
